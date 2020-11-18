37f80ee8ccced531b2b48a126fd0d4c3539dafe2
Manuel Rios <mriosrivas@gmail.com>
2020-11-18 16:00:55 -0600

    Add project 3

--- Common/common_plots.py
+++ Common/common_plots.py
@@ -1,22 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Plot:
-    
+
     def __init__(self):
         pass
 
-
     def plot_multiple(self, signal):
-        """ 
-        Function that plots a impulse/step decomposition of a signal.
-
-        Parameters: 
-        signal (numpy array): Square matrix of impulse/step decomposition 
-        where each row represents a single impulse/step decomposition of each 
-        n-th sample.
         """
-        plt.rcParams["figure.figsize"] = (15,25)
+        Function that plots a impulse/step decomposition of a signal.
+        :param signal: (numpy array) square matrix of impulse/step decomposition where each row 
+        represents a single impulse/step decomposition of each n-th sample.
+        :return: None
+        """
+
+        plt.rcParams["figure.figsize"] = (15, 25)
 
         delta = 1
         y_min = signal.min() - delta
@@ -24,48 +23,62 @@ class Plot:
 
         N = signal.shape[0]
 
-        if (N%2!=0):
-            M = N+1
+        if N % 2 != 0:
+            M = N + 1
         else:
             M = N
 
         for i in range(M):
-            plt.subplot(int(M/2),2,i+1)
+            plt.subplot(int(M / 2), 2, i + 1)
             try:
                 plt.stem(signal[i], use_line_collection=True);
                 plt.ylim((y_min, y_max))
                 plt.title("Sample: {}".format(i))
                 plt.grid()
-            except:
+            finally:
                 pass
-
+        return
 
     def plot_single(self, signal, title="Signal", style='stem'):
-        """ 
-        Function that plots stem of a given signal.
-
-        Parameters: 
-        signal (numpy array): Array of numbers to be plotted.
         """
-
+        Function that plots stem of a given signal.
+        :param signal: (numpy array) array of numbers to be plotted.
+        :param title: (string) title to be used, by default "Signal" is used.
+        :param style: (string)
+        :return: None
+        """
         delta = 1
-        y_min = signal.min() - delta
-        y_max = signal.max() + delta
 
-        plt.rcParams["figure.figsize"] = (10,5)
-        
-        if(style=='stem'):
-            plt.stem(signal[0], use_line_collection=True);
-        elif(style=='line'):
-            plt.plot(signal[0]);
-        plt.ylim((y_min, y_max))
+        plt.rcParams["figure.figsize"] = (10, 5)
+
+        if style == 'stem':
+            plt.stem(signal[0], use_line_collection=True)
+        elif style == 'line':
+            plt.plot(signal[0])
+
+        try:
+            y_min = signal.min() - delta
+            y_max = signal.max() + delta
+            plt.ylim((y_min, y_max))
+        finally:
+            pass
+
         plt.title(title)
         plt.grid()
-        
-        
+        return
+
     def plot_three_signals(self, x1, x2, x3, titles=('x1', 'x2', 'x3'), labels=('sample', 'sample', 'sample')):
-        
-        plt.rcParams["figure.figsize"] = (25,10)
+        """
+        Funtion that allows to plot three signals at the same time.
+        :param x1: (numpy array) signal 1
+        :param x2: (numpy array) signal 2
+        :param x3: (numpy array) signal 3
+        :param titles: (tuple) titles for each signal, by default titles are set to ('x1', 'x2', 'x3').
+        :param labels: (tuple) labels for each signal, by default labels are set to ('sample', 'sample', 'sample').
+        :return: None
+        """
+
+        plt.rcParams["figure.figsize"] = (25, 10)
         plt.subplot(2, 2, 1)
         plt.plot(x1)
         plt.title(titles[0])
@@ -84,3 +97,30 @@ class Plot:
         plt.tight_layout(pad=3.0)
 
         plt.show()
+        return
+
+    def plot_frequency_response(self, x, f, title="Frequency Response of Filter", label=None):
+        """
+        Function to plot power frequency response of a given filter. Power of a signal x is calculated as
+        follows P=20*log(x).
+        :param x: (numpy array) input filter.
+        :param f: (numpy array) normalized frequency between 0 and 0.5.
+        :param title: (string) string of title to use.
+        :param label: (string) string of label to use.
+        :return: None
+        """
+        # We use this to avoid discontinuities in our plot
+        x[0][0] = x[1][0]
+        x[-1][0] = x[-2][0]
+        
+        # Start from here to work
+        # Fill your answer here
+        # Don't touch below
+        
+        plt.plot(f, x_log, label=label)
+        plt.title(title)
+        plt.xlabel("Normalized frequency")
+        plt.ylabel("Gain [dB]")
+        plt.grid("on")
+        return
+
---
+++ Common/fir.py
@@ -0,0 +1,148 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class FIR():
+    def __init__(self):
+        return
+
+    def shifted_sinc(self, fc, M):
+        """
+        Function that performs the calculation of a sinc shifted function.
+        :param fc: (float) cut-off frequency for the low-pass filter. Between 0 and 0.5.
+        :param M: (int) length of the filter kernel. Usually M = 4/BW, where BW is the filter
+                bandwidth of the transition band.
+        :return: (numpy array) sinc shifted time domain response.
+        """
+        
+        # Fill your answer here.
+
+        return h
+
+    def hamming_window(self, M):
+        """
+        Function that calculates a Hamming window of a given M-kernel.
+        :param M: (int) Length of the filter kernel. Usually M = 4/BW, where BW is the filter
+                bandwidth of the transition band.
+        :return: (numpy array) Hamming window of a given M-kernel.
+        """
+
+        # Fill your answer here.
+        
+        return window
+
+    def blackman_window(self, M):
+        """
+        Function that calculates a Blackman window of a given M-kernel.
+        :param M: (int) Length of the filter kernel. Usually M = 4/BW, where BW is the filter
+                bandwidth of the transition band.
+        :return: (numpy array) Blackman window of a given M-kernel.
+        """
+
+        # Fill your answer here.
+        return window
+
+    def window_filter(self, fc, M, normalized=True, window_type='hamming'):
+        """
+        Function to calculate the window filter based on the product of an window and a sinc function.
+        :param fc: (float) cut-off frequency of a low pass filter
+        :param M: (int) Length of the filter kernel. Usually M = 4/BW, where BW is the filter
+                bandwidth of the transition band.
+        :param normalized: (boolean) parameter to set normalized output, by default is set to True.
+        :param window_type: (string) window to use, can be 'hamming' for Hamming window or 'blackman' for
+                Blackman window. By default 'hamming' is used.
+        :return: (numpy array) filter of sinc and window functions of a given M-kernel.
+        """
+        # Fill your answer here.
+        return h
+
+    def spectral_reversal(self, x):
+        """
+        Function that implements the spectral reversal algorithm.
+        :param x: (numpy array) filter of sinc and window functions of a given M-kernel to be spectral reversed.
+        :return: (numpy array) filter of a spectral reversed sinc and window functions of a given M-kernel.
+        """
+        # Fill your answer here.
+
+        return x_reversal
+
+    def spectral_inversion(self, x):
+        """
+        Function that implements the spectral inversion algorithm.
+        :param x: (numpy array) filter of sinc and window functions of a given M-kernel to be spectral inverted.
+        :return: (numpy array) filter of a spectral inverted sinc and window functions of a given M-kernel.
+        """
+        # Fill your answer here.
+        return x_inverted
+
+    def low_pass_filter(self, fc, M, window_type='blackman', normalized=True):
+        """
+        Function that calculates a low pass FIR filter.
+        :param fc: (float) cut-off frequency for the low pass filter.
+        :param M: (int) Length of the filter kernel. Usually M = 4/BW, where BW is the filter
+                bandwidth of the transition band.
+        :param window_type: (string) window to use, can be 'hamming' for Hamming window or 'blackman' for
+                Blackman window. By default 'hamming' is used.
+        :param normalized: (boolean) parameter to set normalized output, by default is set to True.
+        :return: (numpy array) coefficients of a low pass FIR filter of size M.
+        """
+        # Fill your answer here.
+        return low_pass
+
+    def high_pass_filter(self, fc, M, method='spectral_inversion', window_type='blackman', normalized=True):
+        """
+        Function that calculates a high pass FIR filter.
+        :param fc: (float) cut-off frequency for the high pass filter.
+        :param M: (int) Length of the filter kernel. Usually M = 4/BW, where BW is the filter
+                bandwidth of the transition band.
+        :param method: (string) parameter that selects between the 'spectral_inversion' and
+                'spectral_reversal' methods, by default 'spectral_inversion' is used.
+        :param window_type: (string) window to use, can be 'hamming' for Hamming window or 'blackman' for
+                Blackman window. By default 'hamming' is used.
+        :param normalized: (boolean) parameter to set normalized output, by default is set to True.
+        :return: (numpy array) coefficients of a high pass FIR filter of size M.
+        """
+        if method == 'spectral_inversion':
+            # Fill your answer here.
+        elif method == 'spectral_reversal':
+            # Fill your answer here.
+        else:
+            raise Exception("method not defined")
+
+        return high_pass
+
+    def band_filter(self, fc1, fc2, M, band_type='pass', method='spectral_inversion',
+                    window_type='hamming', normalized=True):
+        """
+        Function that calculates a band/reject pass FIR filter.
+        :param fc1: (float) cut-off frequency for the filter.
+        :param fc2: (float) cut-off frequency for the filter.
+        :param M: (int) Length of the filter kernel. Usually M = 4/BW, where BW is the filter
+                bandwidth of the transition band.
+        :param band_type: (string) type of filter, if 'pass' a pass band filter is constructed, if 'reject'
+                a reject band filter is constructed.
+        :param method: (string) parameter that selects between the 'spectral_inversion' and
+                'spectral_reversal' methods, by default 'spectral_inversion' is used.
+        :param window_type: (string) window to use, can be 'hamming' for Hamming window or 'blackman' for
+                Blackman window. By default 'hamming' is used.
+        :param normalized: (boolean) parameter to set normalized output, by default is set to True.
+        :return: (numpy array) coefficients of a band pass (if method='pass') or a reject band
+                (if method='reject') FIR filter of size M.
+        """
+        if fc1 < fc2:
+            f1 = fc1
+            f2 = fc2
+        elif fc1 > fc2:
+            f1 = fc2
+            f2 = fc1
+        else:
+            raise Exception("fc1 and fc2 cannot be equal")
+
+        # Fill your answer here.
+
+        if band_type == 'pass':
+            return self.spectral_inversion(h)
+        elif band_type == 'reject':
+            return h
+        else:
+            raise Exception("incorrect band_type")
